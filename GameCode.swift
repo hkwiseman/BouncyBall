f7228b2a32ef94b6c61380d785b188eb9b7a18c5
Kyle Wiseman <kwiseman@highpoint.edu>
2020-07-28 22:22:06 -0400

    Added game rules, set target position, and added reset

--- GameCode.swift
+++ GameCode.swift
@@ -18,9 +18,19 @@ let funnelPoints = [
     Point(x: 20, y: 0)
 ]
 
+let targetPoints = [
+    Point(x: 10, y: 0),
+    Point(x: 0, y: 10),
+    Point(x: 10, y: 20),
+    Point(x: 20, y: 10)
+]
+
 let barrier = PolygonShape(points: barrierPoints)
 
 let funnel = PolygonShape(points: funnelPoints)
+
+let target = PolygonShape(points: targetPoints)
+
 /*
 The setup() function is called once when the app launches. Without it, your app won't compile.
 Use it to set up and start your app.
@@ -36,6 +46,18 @@ fileprivate func setupBall() {
     scene.add(ball)
     ball.hasPhysics = true
     ball.fillColor = .blue
+    ball.onCollision = ballCollided(with:)
+    ball.isDraggable = false
+    scene.trackShape(ball)
+    ball.onExitedScene = ballExitedScene
+    ball.onTapped = resetGame
+    ball.bounciness = 0.6
+}
+
+func ballCollided(with otherShape: Shape) {
+    if otherShape.name != "target" {return}
+    
+    otherShape.fillColor = .green
 }
 
 fileprivate func setupBarrier() {
@@ -44,6 +66,7 @@ fileprivate func setupBarrier() {
     scene.add(barrier)
     barrier.isImmobile = true
     barrier.fillColor = .darkGray
+    barrier.angle = 0.1
 }
 
 fileprivate func setupFunnel() {
@@ -51,6 +74,18 @@ fileprivate func setupFunnel() {
     scene.add(funnel)
     funnel.onTapped = dropBall
     funnel.fillColor = .blue
+    funnel.isDraggable = false
+}
+
+func setupTarget() {
+    target.position = Point(x: 373, y: 469)
+    target.hasPhysics = true
+    target.isImmobile = true
+    target.isImpermeable = false
+    target.fillColor = .red
+    target.name = "target"
+    scene.add(target)
+    target.isDraggable = false
 }
 
 func setup() {
@@ -60,8 +95,27 @@ func setup() {
     
     setupFunnel()
     
+    setupTarget()
+    
+    resetGame()
+    
+    scene.onShapeMoved = printPosition(of:)
 }
 
 func dropBall() {
     ball.position = funnel.position
+    ball.stopAllMotion()
+    barrier.isDraggable = false
+}
+
+func ballExitedScene() {
+    barrier.isDraggable = true
+}
+
+func resetGame() {
+    ball.position = Point(x: 0, y: -80)
+}
+
+func printPosition(of shape: Shape) {
+    print(shape.position)
 }
