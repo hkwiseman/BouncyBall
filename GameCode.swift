84b9b487812c05a228f3ac6059ca9e26899961c5
Kyle Wiseman <kwiseman@highpoint.edu>
2020-07-28 21:43:49 -0400

    Added touch functionality and refactored code

--- GameCode.swift
+++ GameCode.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-let circle = OvalShape(width: 150, height: 150)
+let ball = OvalShape(width: 40, height: 40)
 let barrierWidth = 300.0
 let barrierHeight = 25.0
 
@@ -11,7 +11,16 @@ let barrierPoints = [
     Point(x: barrierWidth, y: 0)
 ]
 
+let funnelPoints = [
+    Point(x: 0, y: 50),
+    Point(x: 80, y: 50),
+    Point(x: 60, y: 0),
+    Point(x: 20, y: 0)
+]
+
 let barrier = PolygonShape(points: barrierPoints)
+
+let funnel = PolygonShape(points: funnelPoints)
 /*
 The setup() function is called once when the app launches. Without it, your app won't compile.
 Use it to set up and start your app.
@@ -22,14 +31,37 @@ for example if statements and for loops, at the top level; they have to be writt
 of a function.
 */
 
-func setup() {
-    circle.position = Point(x: 250, y: 400)
-    scene.add(circle)
-    circle.hasPhysics = true
-    
+fileprivate func setupBall() {
+    ball.position = Point(x: 250, y: 400)
+    scene.add(ball)
+    ball.hasPhysics = true
+    ball.fillColor = .blue
+}
+
+fileprivate func setupBarrier() {
     barrier.position = Point(x: 200, y: 150)
     barrier.hasPhysics = true
     scene.add(barrier)
     barrier.isImmobile = true
+    barrier.fillColor = .darkGray
+}
+
+fileprivate func setupFunnel() {
+    funnel.position = Point(x: 200, y: scene.height - 25)
+    scene.add(funnel)
+    funnel.onTapped = dropBall
+    funnel.fillColor = .blue
+}
+
+func setup() {
+    setupBall()
     
+    setupBarrier()
+    
+    setupFunnel()
+    
+}
+
+func dropBall() {
+    ball.position = funnel.position
 }
